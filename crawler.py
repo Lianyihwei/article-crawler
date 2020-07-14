798b304d8b0e2401f87cccd8d3c5499d2e5fb007
Lianyihwei <lianyihwei@gmail.com>
2020-07-14 14:11:10 +0800

    fixed version

--- crawler.py
+++ crawler.py
@@ -1,14 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.action_chains import ActionChains
 from bs4 import BeautifulSoup
 import time
-from datetime import datetime
 import os
-import traceback
+from datetime import date
 import pymysql.cursors
-from dotenv import load_dotenv
-load_dotenv()
+import urllib.request
+from datetime import date
+today = str(date.today())
 
 connection = pymysql.connect(host='localhost',
                              user='user',
@@ -17,7 +16,7 @@ connection = pymysql.connect(host='localhost',
                              cursorclass=pymysql.cursors.DictCursor)
 
 options = Options()
-options.add_argument("--disable-notifications")
+options.add_argument('--headless')
 options.experimental_options["prefs"] = {"profile.default_content_settings": {"images": 2},
                                         "profile.managed_default_content_settings": {"images":2}}
 
@@ -25,129 +24,92 @@ driver = webdriver.Chrome("/Users/lianyihwei/Applications/webDrive/chromedriver"
 
 try:
     with connection.cursor() as cursor:
-        driver.get('https://www.facebook.com')
-        time.sleep(1)
-        username = driver.find_elements_by_css_selector("input[name=email]")[0]
-        password = driver.find_elements_by_css_selector("input[name=pass]")[0]
-        username.send_keys(os.getenv("EMAIL"))
-        password.send_keys(os.getenv("PASSWORD"))
-        time.sleep(2)
-        login_button = driver.find_elements_by_css_selector("input[type=submit]")[0]
-        login_button.click()
-        time.sleep(2)
-        
-        driver.get("https://www.facebook.com/ericsteakshop")
-        for i in range(1,4):
+        for i in range(1,2):
+# --------Inside--------
+#             time.sleep(2)
+            driver.get('https://www.inside.com.tw/?page='+str(i))
+            soup = BeautifulSoup(driver.page_source)
+            article_box = soup.select("div.post_list-list_style")[0]
+            articles = article_box.select("div.post_list_item")
+            for article in articles:
+                title = article.select("h3.post_title")[0].text
+                date = article.select("li.post_date")[0].text.strip().replace("/","-")
+                tags = article.select("a.hero_slide_tag")
+                tags_string = ""
+                for tag in tags:
+                    tags_string += "#"+tag.text+" "
+                if today == date: 
+                    print(title)
+                    print(date)
+                    print(tags_string)
+                    
+                    sql = '''
+                    INSERT INTO `new_media`.`articles`(`title`,`date`,`tags`,`brand`)
+                    VALUE("{}","{}","{}","{}")
+                    '''.format(title,date,tags_string,"inside")
+                    cursor.execute(sql)
+                    connection.commit()
+# --------TechNews--------
+#             time.sleep(2)
+            driver.get('https://technews.tw/page/'+ str(i)+ '/')
+            soup = BeautifulSoup(driver.page_source)
+            article_box = soup.select("div#content")[0]
+            articles = article_box.select("header.entry-header")
+            for article in articles:
+                title = article.select("h1.entry-title")[0].text
+                date = article.select("span.body")[1].text.strip().replace(" 年 ","-").replace(" 月 ","-").replace(" 日 ","-")
+                tags = article.select("span.body")[2].select("a")
+                iframe = article.select("iframe")[1]
+                response = urllib.request.urlopen(iframe.attrs["src"])
+                iframe_soup = BeautifulSoup(response)
+                share = iframe_soup.select("span#u_0_2")[0].text
+                tags_string = ""
+                for tag in tags:
+                    tags_string += "#"+tag.text+" "
+                if today == date[0:10]:
+                    print(title)
+                    print(date)
+                    print(tags_string)
+                    print(share)
+                    
+
+                    sql = '''
+                    INSERT INTO `new_media`.`articles`(`title`,`date`,`tags`,`share`,`brand`)
+                    VALUE("{}","{}","{}","{}","{}")
+                    '''.format(title,date,tags_string,share,"technews")
+                    cursor.execute(sql)
+                    connection.commit()            
+# --------TechOrange--------
+        driver.get("https://buzzorange.com/techorange/")
+        for i in range(1,2):
             driver.execute_script("window.scrollTo(0,document.body.scrollHeight);")
             time.sleep(2)
-        
-        check = True
-        while check:
-# -------點選打開更多留言-------
-            show_more_comments = driver.find_elements_by_css_selector("a._4sxc._42ft")
-            for show_more_comment in show_more_comments:
-                ActionChains(driver).move_to_element(show_more_comment).perform()
-                show_more_comment.click()
-# -------點選打開留言中更多內容-------
-            show_more_cotents = driver.find_elements_by_css_selector("a._5v47.fss")
-            for show_more_cotent in show_more_cotents:
-                ActionChains(driver).move_to_element(show_more_cotent).perform()
-                show_more_cotent.click()
-            
-            time.sleep(2)
-            print("open reply")
-            
-            if (len(show_more_comments) == 0 and len(show_more_cotents) == 0):
-                check = False
-
-# -------抓取貼文內容-------
-        
         soup = BeautifulSoup(driver.page_source)
-        article_box = soup.select("div._1xnd")[1]
-        articles = article_box.select("div._5pcr.userContentWrapper")
+        article_box = soup.select("main#main")[0]
+        articles = article_box.select("article")
         for article in articles:
-            
-            date = content = promotion_date = ""
-            attach = interactive = promotion_attach = promotion_interactive = replied_count = shared_count = good = 0
-            
-            utime = article.select("abbr._5ptz")[0]["data-utime"]
-            date = datetime.fromtimestamp(int(utime))
-            print("date: "+str(date))
-            
-            content = ""
-            if(len(article.select("div.text_exposed_root")) == 0):
-                show_contents = article.select("div.userContent > p")
-                for show_content in show_contents:
-                    content = content + show_contents.text + "。"
+            title = article.select("h4.entry-title")[0].text
+            date = article.select("time.entry-date")[0].text.strip().replace("/","-")
+            share = article.select("span.shareCount")[0].text.split(" ")[0]
+            if share.find("K") != -1:
+                share = float(share) * 1000
             else:
-                show_contents = article.select("div.text_exposed_root > p")
-                hide_contents = article.select("div.text_exposed_show > p")
-                for show_content in show_contents:
-                    content = content + show_content.text +"。"
-                for hide_content in hide_contents:
-                    content = content +hide_content.text +"。"
-            print("Content: " + content)
-            
-# -------抓取廣告數據 attach觸及人數 interactive互動人數 
-            if(len(article.select("tr._51mx")) > 0):
-                attach = article.select("td._51m-.vMid.hLeft span")[0].text.replace(",","")
-                interactive = article.select("td._51mw._51m-.vMid.hLeft span")[0].text.replace(",","")
-                print("觸及人數: " + attach)
-                print("互動人數: " + interactive)
-# -------promotion加強推廣日期、觸及與互動人數-------
-                if(len(article.select("div._6r-l div._one.lfloat div")) > 0):
-                    promotion_string = article.select("div._6r-l div._one.lfloat div")[0].text
-                    promotion_date = promotion_string.split(":")[1]
-                    promotion_attach = article.select("div._6r-n")[0].text.replace(",","")
-                    promotion_interactive = article.select("div._6r-n")[1].text.replace(",","")
-                    print("promotion_date: " + promotion_date)
-                    print("promotion_attach: " + promotion_attach)
-                    print("promotion_interactive: " + promotion_interactive)
-# -------按讚、留言、分享數-------
-            good = article.select("span._81hb")[0].text.replace(",","")
-            relpied_elements = article.select("span._1whp._4vn2")
-            shared_elements = article.select("span._355t._4vn2")
-            print("按讚數： "+good)
-            if(len(relpied_elements) > 0):
-                replied_count = relpied_elements[0].text.split("則")[0].replace(",","")
-                print("留言數： "+replied_count)
-            if(len(shared_elements) > 0):
-                shared_count = shared_elements[0].text.split("次")[0].replace(",","")
-                print("分享數： "+shared_count)
-            
-            sql = '''SELECT * FROM fb_social_data.posts WHERE date = "{}"
-                '''.format(date)
-            cursor.execute(sql)
-            posts = cursor.fetchall()
-            
-            if(len(posts) > 0):
-                sql = '''UPDATE `fb_social_data`.`posts` SET 
-                        date = "{}",
-                        content = "{}",
-                        attach = "{}",
-                        interactive = "{}",
-                        promotion_date = "{}",
-                        promotion_attach = "{}",
-                        promotion_interactive = "{}",
-                        replied_count = "{}",
-                        shared_count = "{}",
-                        good = "{}" WHERE (date = "{}")'''.format(
-                date,content,attach,interactive,promotion_date,promotion_attach,promotion_interactive,
-                replied_count,shared_count,good,date)
-                
-            else:
-                sql = '''INSERT INTO fb_social_data.posts(date,content,attach,interactive,promotion_date,promotion_attach,
-                        promotion_interactive,replied_count,shared_count,good) 
-                        VALUES("{}","{}","{}","{}","{}","{}","{}","{}","{}","{}")'''.format(
-                date,content,attach,interactive,promotion_date,promotion_attach,promotion_interactive,
-                replied_count,shared_count,good)
+                share = article.select("span.shareCount")[0].text.split(" ")[0]
+            if today == date:
+                print(title)
+                print(date)
+                print(share)
 
-            cursor.execute(sql)
-            connection.commit()
-                    
+                sql = '''
+                INSERT INTO `new_media`.`articles`(`title`,`date`,`share`,`brand`)
+                VALUE("{}","{}","{}","{}")
+                '''.format(title,date,share,"techorange")
+                cursor.execute(sql)
+                connection.commit()
+
+
     connection.close()
     driver.close()
-except Exception:
-    print(traceback.format_exc())
-    connection.close()
+except Exception as e:
+    print(e)
     driver.close()
